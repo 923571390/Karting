6a2f850c77748f79a8e5ed7902a08de11716cbcf
923571390@qq.com <923571390@qq.com>
2020-12-31 16:00:28 +0800

    UI

---
+++ HappyKarting(工程)/Assets/Karting/Scripts/Win.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if(ddd.stage<4) ddd.stage++;
+    }
+
+}
---
+++ HappyKarting(工程)/Assets/Karting/Scripts/ddd.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ddd : MonoBehaviour
+{
+    static ddd _instance;
+    public static int stage = 1;
+    // Use this for initialization
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void setStage(int a)
+    {
+        stage = a;
+    }
+
+    public int getStage()
+    {
+        return stage;
+    }
+}
