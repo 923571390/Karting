4529778bbe71f6a9f130d722280d16bdcaf485ce
923571390@qq.com <923571390@qq.com>
2021-01-02 16:51:24 +0800

    完成

--- HappyKartingProject/Assets/Karting/Scripts/Win.cs
+++ HappyKartingProject/Assets/Karting/Scripts/Win.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Win : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject forward;
+    public GameObject backward;
     private void Start()
     {
         if(ddd.stage<4) ddd.stage++;
+        else
+        {
+            backward.SetActive(true);
+            forward.SetActive(false);
+        }
     }
 
 }
